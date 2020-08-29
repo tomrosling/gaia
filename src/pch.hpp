f227cdcf36cc570a577c79eb5bd7bb113d872be9
Tom Rosling <tom.rosling@gmail.com>
2020-08-29 15:37:50 +0100

    Added glm dependency
    and renamed thirdparty/ -> dependencies/

--- src/pch.hpp
+++ src/pch.hpp
@@ -7,16 +7,32 @@
 #include <windows.h>
 #include <wrl/client.h>
 
+#undef max
+#undef min
+
 #include <DirectXMath.h>
 #include <d3d12.h>
 #include <d3dx12.h>
 #include <d3dcompiler.h>
 #include <dxgi1_6.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtx/norm.hpp>
+
 namespace gaia
 {
 
 using Microsoft::WRL::ComPtr;
+
+using Vec2f = glm::vec2;
+using Vec3f = glm::vec3;
+using Vec4f = glm::vec4;
+using Mat3f = glm::mat3;
+using Mat4f = glm::mat4;
+using Quatf = glm::quat;
+
+// Alias namespace used for global functions (dot, cross, etc).
+namespace math = glm;
 
 inline void DebugOut(const char* fmt, ...)
 {
@@ -25,7 +41,7 @@ inline void DebugOut(const char* fmt, ...)
     int size = vsnprintf(nullptr, 0, fmt, args);
     char* buf = (char*)alloca(size);
     vsnprintf(buf, size, fmt, args);
-    OutputDebugStringA(buf);
+    ::OutputDebugStringA(buf);
     va_end(args);
 }
 
