983608640413277bf9c86dba60ac2d22daeee26e
Tom Rosling <tom.rosling@gmail.com>
2020-09-05 19:44:22 +0100

    Added tiling to terrain
    Replaced rand() high frequency noise with another Perlin octave, because it's easier to seed (which is important when matching up tile borders)

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -7,70 +7,136 @@
 namespace gaia
 {
 
-const int CellsX = 255;
-const int CellsZ = 255;
-const int NumVerts = (CellsX + 1) * (CellsZ + 1);
-const int NumIndices = 2 * 3 * CellsX * CellsZ;
+const int NumTilesX = 2;
+const int NumTilesZ = 2;
+const int CellsPerTileX = 255;
+const int CellsPerTileZ = 255;
+const int VertsPerTile = (CellsPerTileX + 1) * (CellsPerTileZ + 1);
+const int IndicesPerTile = 2 * 3 * CellsPerTileX * CellsPerTileZ;
 const float CellSize = 0.05f;
-static_assert(NumVerts <= (1 << 16), "Index format too small");
+static_assert(VertsPerTile <= (1 << 16), "Index format too small");
 
-static int VertexAddress(int x, int z) 
+
+static int VertexIndex(int x, int z) 
 {
-    assert(0 <= x && x <= CellsX);
-    assert(0 <= z && z <= CellsZ);
-    return (CellsX + 1) * z + x;
+    assert(0 <= x && x <= CellsPerTileX);
+    assert(0 <= z && z <= CellsPerTileZ);
+    return (CellsPerTileX + 1) * z + x;
+}
+
+static int TileIndex(int x, int z)
+{
+    assert(0 <= x && x <= NumTilesX);
+    assert(0 <= z && z <= NumTilesZ);
+    return (NumTilesX) * z + x;
 }
 
 static Vec3f TriangleNormal(const Vec3f& p0, const Vec3f& p1, const Vec3f& p2)
 {
     return math::normalize(math::cross(p1 - p0, p2 - p0));
-};
+}
+
+// TODO: We could just write directly to the intermediate buffer when mapped.
+static void CreateVertexBuffer(Renderer& renderer, Terrain::VertexBuffer& vb, int vertexCount, const void* data)
+{
+    renderer.CreateBuffer(vb.buffer, vb.intermediateBuffer, vertexCount * sizeof(Vertex), data);
+    vb.view.BufferLocation = vb.buffer->GetGPUVirtualAddress();
+    vb.view.SizeInBytes = vertexCount * sizeof(Vertex);
+    vb.view.StrideInBytes = sizeof(Vertex);
+}
+
+static void CreateIndexBuffer(Renderer& renderer, Terrain::IndexBuffer& ib, int indexCount, const void* data)
+{
+    renderer.CreateBuffer(ib.buffer, ib.intermediateBuffer, indexCount * sizeof(uint16_t), data);
+    ib.view.BufferLocation = ib.buffer->GetGPUVirtualAddress();
+    ib.view.Format = DXGI_FORMAT_R16_UINT;
+    ib.view.SizeInBytes = indexCount * sizeof(uint16_t);
+}
+
 
 void Terrain::Build(Renderer& renderer)
 {
+    renderer.BeginUploads();
+
     // Note: rand() is not seeded so this is still deterministic, for now.
     int seed = rand();
 
-    auto vertexData = std::make_unique<Vertex[]>(NumVerts);
-    auto indexData = std::make_unique<uint16_t[]>(NumIndices);
+    m_tileVertexBuffers.resize(NumTilesX * NumTilesZ);
+    for (int z = 0; z < NumTilesZ; ++z)
+    {
+        for (int x = 0; x < NumTilesX; ++x)
+        {
+            BuildTile(renderer, x, z, seed);
+        }
+    }
+
+    BuildIndexBuffer(renderer);
+    BuildWater(renderer);
+
+    renderer.EndUploads();
+}
+
+void Terrain::Render(Renderer& renderer)
+{
+    ID3D12GraphicsCommandList2& commandList = renderer.GetDirectCommandList();
+    commandList.IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+    // Render the terrain itself.
+    for (const VertexBuffer& vertexBuffer : m_tileVertexBuffers)
+    {
+        commandList.IASetVertexBuffers(0, 1, &vertexBuffer.view);
+        commandList.IASetIndexBuffer(&m_indexBuffer.view);
+        commandList.DrawIndexedInstanced(IndicesPerTile, 1, 0, 0, 0);
+    }
+
+    // Render "water".
+    commandList.IASetVertexBuffers(0, 1, &m_waterVertexBuffer.view);
+    commandList.IASetIndexBuffer(&m_waterIndexBuffer.view);
+    commandList.DrawIndexedInstanced(m_waterIndexBuffer.view.SizeInBytes / sizeof(uint16_t), 1, 0, 0, 0);
+}
+
+void Terrain::BuildIndexBuffer(Renderer& renderer)
+{
+    // TODO: Consider triangle strips or other topology?
+    auto indexData = std::make_unique<uint16_t[]>(IndicesPerTile);
+    for (int z = 0; z < CellsPerTileZ; ++z)
+    {
+        for (int x = 0; x < CellsPerTileX; ++x)
+        {
+            uint16_t* p = &indexData[2 * 3 * (CellsPerTileX * z + x)];
+            p[0] = (CellsPerTileX + 1) * (z + 0) + (x + 0);
+            p[1] = (CellsPerTileX + 1) * (z + 1) + (x + 0);
+            p[2] = (CellsPerTileX + 1) * (z + 1) + (x + 1);
+            p[3] = (CellsPerTileX + 1) * (z + 0) + (x + 0);
+            p[4] = (CellsPerTileX + 1) * (z + 1) + (x + 1);
+            p[5] = (CellsPerTileX + 1) * (z + 0) + (x + 1);
+        }
+    }
+
+    CreateIndexBuffer(renderer, m_indexBuffer, IndicesPerTile, indexData.get());
+}
+
+void Terrain::BuildTile(Renderer& renderer, int tileX, int tileZ, int seed)
+{
+    assert(0 <= tileX && tileX < NumTilesX);
+    assert(0 <= tileZ && tileZ < NumTilesZ);
+
+    auto vertexData = std::make_unique<Vertex[]>(VertsPerTile);
 
     // Initialise pos, col / generate heights
-    for (int z = 0; z <= CellsZ; ++z)
+    for (int z = 0; z <= CellsPerTileZ; ++z)
     {
-        for (int x = 0; x <= CellsX; ++x)
+        for (int x = 0; x <= CellsPerTileX; ++x)
         {
-            const struct
-            {
-                float frequency;
-                float amplitude;
-            } Octaves[] = { 
-                { 1.f / 131.f, 3.f },
-                { 1.f / 51.f, 1.f },
-                { 1.f / 13.f, 0.15f }
-            };
+            int globalX = x + tileX * CellsPerTileX;
+            int globalZ = z + tileZ * CellsPerTileZ;
 
-            Vertex& v = vertexData[VertexAddress(x, z)];
-            float height = 0.5f;
+            Vertex& v = vertexData[VertexIndex(x, z)];
+            v.position = GeneratePos(globalX, globalZ, seed);
 
-            // May be better just to use one of the built in stb_perlin functions
-            // to generate multiple octaves, but this is quite nice for now.
-            for (int i = 0; i < (int)std::size(Octaves); ++i)
-            {
-                auto [frequency, amplitude] = Octaves[i];
-                height += amplitude * stb_perlin_noise3_seed((float)x * frequency, 0.f, (float)z * frequency, 0, 0, 0, seed + i);
-            }
+            // Leave v.normal uninitialised until below...
 
-            // Cheaply add a little bit of texture.
-            // TODO: Remove and add normal map (and just diffuse textures).
-            height += 0.01f * (float)rand() / (float)RAND_MAX;
-            
-            v.position.x = CellSize * ((float)x - 0.5f * (float)CellsX);
-            v.position.y = height;
-            v.position.z = CellSize * ((float)z - 0.5f * (float)CellsZ);
-
-            // Leave v.normal uninitialised for now...
-
-            float t = std::clamp(height - 0.8f, 0.f, 1.f);
+            float t = std::clamp(v.position.y - 0.8f, 0.f, 1.f);
             v.colour[0] = math::Lerp(0x00, 0xff, t);
             v.colour[1] = math::Lerp(0x80, 0xff, t);
             v.colour[2] = math::Lerp(0x00, 0xff, t);
@@ -79,97 +145,42 @@ void Terrain::Build(Renderer& renderer)
     }
 
     // Generate normals
-    for (int z = 0; z <= CellsZ; ++z)
+    for (int z = 0; z <= CellsPerTileZ; ++z)
     {
-        for (int x = 0; x <= CellsX; ++x)
+        for (int x = 0; x <= CellsPerTileX; ++x)
         {
-            Vertex& v = vertexData[VertexAddress(x, z)];
+            Vertex& v = vertexData[VertexIndex(x, z)];
+            int globalX = x + tileX * CellsPerTileX;
+            int globalZ = z + tileZ * CellsPerTileZ;
 
-            Vec3f normal = { 0.f, 0.f, 0.f };
-            if (x > 0)
-            {
-                const Vec3f& left = vertexData[VertexAddress(x - 1, z)].position;
-                if (z > 0)
-                {
-                    // Below left
-                    const Vec3f& down = vertexData[VertexAddress(x, z - 1)].position;
-                    const Vec3f& downLeft = vertexData[VertexAddress(x - 1, z - 1)].position;
-                    normal += TriangleNormal(downLeft, left, v.position);
-                    normal += TriangleNormal(downLeft, v.position, down);
-                }
-                if (z < CellsZ)
-                {
-                    // Above left
-                    const Vec3f& up = vertexData[VertexAddress(x, z + 1)].position;
-                    normal += TriangleNormal(left, up, v.position);
-                }
-            }
+            // Make sure seams have consistent normals by resampling the Perlin noise when the neighbouring vertices don't exist.
+            // We could avoid this by adding an unrendered border to each tile, and just sampling its height.
+            Vec3f left = (x > 0) ? vertexData[VertexIndex(x - 1, z)].position : GeneratePos(globalX - 1, globalZ, seed);
+            Vec3f down = (z > 0) ? vertexData[VertexIndex(x, z - 1)].position : GeneratePos(globalX, globalZ - 1, seed);
+            Vec3f right = (x < CellsPerTileX) ? vertexData[VertexIndex(x + 1, z)].position : GeneratePos(globalX + 1, globalZ, seed);
+            Vec3f up    = (z < CellsPerTileZ) ? vertexData[VertexIndex(x, z + 1)].position : GeneratePos(globalX, globalZ + 1, seed);
+            Vec3f downLeft = (x > 0 && z > 0) ? vertexData[VertexIndex(x - 1, z - 1)].position : GeneratePos(globalX - 1, globalZ - 1, seed);
+            Vec3f upRight = (x < CellsPerTileX && z < CellsPerTileZ) ? vertexData[VertexIndex(x + 1, z + 1)].position : GeneratePos(globalX + 1, globalZ + 1, seed);
 
-            if (x < CellsX)
-            {
-                const Vec3f& right = vertexData[VertexAddress(x + 1, z)].position;
-                if (z > 0)
-                {
-                    // Below right
-                    const Vec3f& down = vertexData[VertexAddress(x, z - 1)].position;
-                    normal += TriangleNormal(down, v.position, right);
-                }
-                if (z < CellsZ)
-                {
-                    // Above right
-                    const Vec3f& up = vertexData[VertexAddress(x, z + 1)].position;
-                    const Vec3f& upRight = vertexData[VertexAddress(x + 1, z + 1)].position;
-                    normal += TriangleNormal(v.position, up, upRight);
-                    normal += TriangleNormal(v.position, upRight, right);
-                }
-            }
-
-            v.normal = math::normalize(normal);
+            Vec3f normal = Vec3fZero;
+            normal += TriangleNormal(downLeft, left, v.position);
+            normal += TriangleNormal(downLeft, v.position, down);
+            normal += TriangleNormal(left, up, v.position);
+            normal += TriangleNormal(down, v.position, right);
+            normal += TriangleNormal(v.position, up, upRight);
+            normal += TriangleNormal(v.position, upRight, right);
+            v.normal = normal / 6.f;
         }
     }
 
-    // Init indices
-    // TODO: Consider triangle strips or other topology?
-    for (int z = 0; z < CellsZ; ++z)
-    {
-        for (int x = 0; x < CellsX; ++x)
-        {
-            uint16_t* p = &indexData[2 * 3 * (CellsX * z + x)];
-            p[0] = (CellsX + 1) * (z + 0) + (x + 0);
-            p[1] = (CellsX + 1) * (z + 1) + (x + 0);
-            p[2] = (CellsX + 1) * (z + 1) + (x + 1);
-            p[3] = (CellsX + 1) * (z + 0) + (x + 0);
-            p[4] = (CellsX + 1) * (z + 1) + (x + 1);
-            p[5] = (CellsX + 1) * (z + 0) + (x + 1);
-        }
-    }
+    int tileIndex = TileIndex(tileX, tileZ);
+    CreateVertexBuffer(renderer, m_tileVertexBuffers[tileIndex], VertsPerTile, vertexData.get());
+}
 
-    renderer.BeginUploads();
-
-    // TODO: just keep a vector (or static array) on Renderer of intermediate buffers, cleared on EndUploads
-    auto createVertexBuffer = [](Renderer& renderer, VertexBuffer& vb, ComPtr<ID3D12Resource>& intermediateBuffer, int vertexCount, const void* data)
-    {
-        renderer.CreateBuffer(vb.buffer, intermediateBuffer, vertexCount * sizeof(Vertex), data);
-        vb.view.BufferLocation = vb.buffer->GetGPUVirtualAddress();
-        vb.view.SizeInBytes = vertexCount * sizeof(Vertex);
-        vb.view.StrideInBytes = sizeof(Vertex);
-    };
-
-    auto createIndexBuffer = [](Renderer& renderer, IndexBuffer& ib, ComPtr<ID3D12Resource>& intermediateBuffer, int indexCount, const void* data) {
-        renderer.CreateBuffer(ib.buffer, intermediateBuffer, indexCount * sizeof(uint16_t), data);
-        ib.view.BufferLocation = ib.buffer->GetGPUVirtualAddress();
-        ib.view.Format = DXGI_FORMAT_R16_UINT;
-        ib.view.SizeInBytes = indexCount * sizeof(uint16_t);
-    };
-
-    // Main terrain data
-    ComPtr<ID3D12Resource> intermediateVB;
-    createVertexBuffer(renderer, m_vertexBuffer, intermediateVB, NumVerts, vertexData.get());
-    ComPtr<ID3D12Resource> intermediateIB;
-    createIndexBuffer(renderer, m_indexBuffer, intermediateIB, NumIndices, indexData.get());
-
-    const float HalfGridSizeX = 0.5f * CellSize * (float)CellsX;
-    const float HalfGridSizeZ = 0.5f * CellSize * (float)CellsZ;
+void Terrain::BuildWater(Renderer& renderer)
+{
+    const float HalfGridSizeX = 0.5f * CellSize * (float)(CellsPerTileX * NumTilesX);
+    const float HalfGridSizeZ = 0.5f * CellSize * (float)(CellsPerTileZ * NumTilesZ);
     const Vertex WaterVerts[] = {
         { { -HalfGridSizeX, 0.f, -HalfGridSizeZ }, Vec3fY, { 0x20, 0x70, 0xff, 0x80 } },
         { { -HalfGridSizeX, 0.f,  HalfGridSizeZ }, Vec3fY, { 0x20, 0x70, 0xff, 0x80 } },
@@ -182,29 +193,38 @@ void Terrain::Build(Renderer& renderer)
         0, 2, 3
     };
 
-    // Water plane
-    ComPtr<ID3D12Resource> intermediateWaterVB;
-    createVertexBuffer(renderer, m_waterVertexBuffer, intermediateWaterVB, (int)std::size(WaterVerts), WaterVerts);
-    ComPtr<ID3D12Resource> intermediateWaterIB;
-    createIndexBuffer(renderer, m_waterIndexBuffer, intermediateWaterIB, (int)std::size(WaterIndices), WaterIndices);
-
-    renderer.EndUploads();
+    CreateVertexBuffer(renderer, m_waterVertexBuffer, (int)std::size(WaterVerts), WaterVerts);
+    CreateIndexBuffer(renderer, m_waterIndexBuffer, (int)std::size(WaterIndices), WaterIndices);
 }
 
-void Terrain::Render(Renderer& renderer)
+Vec3f Terrain::GeneratePos(int globalX, int globalZ, int seed)
 {
-    ID3D12GraphicsCommandList2& commandList = renderer.GetDirectCommandList();
-    commandList.IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    const struct
+    {
+        float frequency;
+        float amplitude;
+    } Octaves[] = {
+        { 1.f / 131.f, 3.f },
+        { 1.f / 51.f, 1.f },
+        { 1.f / 13.f, 0.15f },
+        { 0.9999f, 0.05f }
+    };
 
-    // Render the terrain itself.
-    commandList.IASetVertexBuffers(0, 1, &m_vertexBuffer.view);
-    commandList.IASetIndexBuffer(&m_indexBuffer.view);
-    commandList.DrawIndexedInstanced(NumIndices, 1, 0, 0, 0);
+    float height = 0.5f;
 
-    // Render "water".
-    commandList.IASetVertexBuffers(0, 1, &m_waterVertexBuffer.view);
-    commandList.IASetIndexBuffer(&m_waterIndexBuffer.view);
-    commandList.DrawIndexedInstanced(m_waterIndexBuffer.view.SizeInBytes / sizeof(uint16_t), 1, 0, 0, 0);
+    // May be better just to use one of the built in stb_perlin functions
+    // to generate multiple octaves, but this is quite nice for now.
+    for (int i = 0; i < (int)std::size(Octaves); ++i)
+    {
+        auto [frequency, amplitude] = Octaves[i];
+        height += amplitude * stb_perlin_noise3_seed((float)globalX * frequency, 0.f, (float)globalZ * frequency, 0, 0, 0, seed + i);
+    }
+
+    return Vec3f(
+        CellSize * ((float)globalX - 0.5f * (float)(CellsPerTileX * NumTilesX)),
+        height,
+        CellSize * ((float)globalZ - 0.5f * (float)(CellsPerTileZ * NumTilesZ))
+    );
 }
 
 }
--- src/Terrain.hpp
+++ src/Terrain.hpp
@@ -9,24 +9,32 @@ struct Vertex;
 class Terrain
 {
 public:
-    void Build(Renderer& renderer);
-    void Render(Renderer& renderer);
-
-private:
     struct VertexBuffer
     {
         ComPtr<ID3D12Resource> buffer;
-        D3D12_VERTEX_BUFFER_VIEW view = {};        
+        ComPtr<ID3D12Resource> intermediateBuffer;
+        D3D12_VERTEX_BUFFER_VIEW view = {};
     };
 
     struct IndexBuffer
     {
         ComPtr<ID3D12Resource> buffer;
+        ComPtr<ID3D12Resource> intermediateBuffer;
         D3D12_INDEX_BUFFER_VIEW view = {};
     };
 
-    VertexBuffer m_vertexBuffer;
-    IndexBuffer m_indexBuffer;
+    void Build(Renderer& renderer);
+    void Render(Renderer& renderer);
+
+private:
+    void BuildIndexBuffer(Renderer& renderer);
+    void BuildTile(Renderer& renderer, int tileX, int tileZ, int seed);
+    void BuildWater(Renderer& renderer);
+    Vec3f GeneratePos(int globalX, int globalZ, int seed);
+
+    std::vector<VertexBuffer> m_tileVertexBuffers;
+    IndexBuffer m_indexBuffer; // Indices are the same for each tile.
+
     VertexBuffer m_waterVertexBuffer;
     IndexBuffer m_waterIndexBuffer;
 };
