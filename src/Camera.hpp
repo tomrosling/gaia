852cff950601805786a809ac3ceacc166e2ac241
Tom Rosling <tom.rosling@gmail.com>
2021-04-25 13:59:24 +0100

    Increased camera and terrain mod speeds.

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -13,11 +13,11 @@ public:
 
 private:
     // State
-    Vec3f m_pos{ 0.f, 3.f, 0.f };
-    Vec2f m_rot = Vec2fZero;
+    Vec3f m_pos{ 0.f, 15.f, 0.f };
+    Vec2f m_rot{ -0.707, 0.f };
 
     // Config
-    float m_linSpeed = 3.f;
+    float m_linSpeed = 8.f;
     Vec2f m_rotSpeed{ 0.0035f, 0.003f }; // Radians per pixel of mouse movement.
 };
 
